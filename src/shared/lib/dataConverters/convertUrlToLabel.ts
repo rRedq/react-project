baadd19de011e1d2adb82b14217585c356de4a24
rRedq <redq_000@mail.ru>
2024-07-07 12:15:17 +0500

    fix: fix images

--- src/shared/lib/dataConverters/convertUrlToLabel.ts
+++ src/shared/lib/dataConverters/convertUrlToLabel.ts
@@ -5,7 +5,7 @@ export const convertUrlToLabel = (url: string): CategoriesType => {
   const splitedUrl: string[] = url.split('/');
   const lastElem: string = splitedUrl[splitedUrl.length - 1];
   const resultWithExt: string = lastElem.split('?')[0];
-  const resultWithoutExt: string = resultWithExt.split('.')[0];
+  const resultWithoutExt: string = resultWithExt.split('.')[0].split('-')[0];
 
   const result: CategoriesType = validCategories.includes(
     resultWithoutExt as CategoriesType
