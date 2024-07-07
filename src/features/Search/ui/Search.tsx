8dec9a4aef8778a261ca92259eacd65f87833832
rRedq <redq_000@mail.ru>
2024-07-07 09:59:31 +0500

    feat: add search icon

--- src/features/Search/ui/Search.tsx
+++ src/features/Search/ui/Search.tsx
@@ -1,7 +1,8 @@
 import { ChangeEvent, Component, FormEvent, ReactNode } from 'react';
 import style from './Search.module.scss';
 import { ErrorButton } from 'shared/lib/ui/ErrorButton';
-
+import searchIcon from 'shared/assets/images/images/search.svg';
+import { getLocalState } from 'shared/lib/localState';
 interface SearchState {
   value: string;
 }
@@ -14,24 +15,45 @@ export class Search extends Component<SearchProps, SearchState> {
   updateSearch = this.props.updateSearch;
   state: SearchState = { value: '' };
 
+  componentDidMount(): void {
+    const value: string = getLocalState('search') || '';
+    this.setState({ value });
+  }
+
   onSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     this.updateSearch(this.state.value);
   };
 
+  crossClick = (): void => {
+    this.setState({ value: '' }, () => this.updateSearch(this.state.value));
+  };
+
   render(): ReactNode {
+    const { value } = this.state;
     return (
       <form className={style.searchForm} onSubmit={this.onSubmit}>
         <ErrorButton />
-        <input
-          className={style.search}
-          type="text"
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            this.setState({ value: e.target.value })
-          }
-          value={this.state.value}
-        />
-        <input type="submit" />
+        <div className={style.cover}>
+          <input
+            className={style.search}
+            type="text"
+            placeholder="search"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              this.setState({ value: e.target.value })
+            }
+            value={this.state.value}
+          />
+          {value && (
+            <div className={style.cross} onClick={this.crossClick}>
+              X
+            </div>
+          )}
+        </div>
+
+        <button className={style.btn} type="submit">
+          <img src={searchIcon} />
+        </button>
       </form>
     );
   }
