2d94ce087aadbd9ffecb09797b1e289a875a7828
rRedq <redq_000@mail.ru>
2024-07-26 21:20:28 +0500

    chore: add image path for next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'starwars-visualguide.com',
+        pathname: '/assets/img/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
