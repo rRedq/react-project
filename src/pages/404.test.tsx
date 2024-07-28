fe205e6200f56053613d14d9f7087adab04ae6f5
rRedq <redq_000@mail.ru>
2024-07-28 18:32:15 +0500

    refactor: update tests

--- src/pages/404.test.tsx
+++ src/pages/404.test.tsx
@@ -1,8 +1,8 @@
-import { NotFound } from './NotFound';
 import { render } from '@testing-library/react';
+import Custom404 from './404';
 
 test('testing NotFound', () => {
-  const { getByText } = render(<NotFound />);
+  const { getByText } = render(<Custom404 />);
 
   const notFound = getByText(/Page hasn't been found/i);
   expect(notFound).toBeInTheDocument();
