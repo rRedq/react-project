e71ebe7536512bfc4fff143f594bc552353fc9a0
rRedq <redq_000@mail.ru>
2024-08-08 08:54:02 +0500

    chore: change config according api router

--- setupTests.ts
+++ setupTests.ts
@@ -1,6 +1,21 @@
 import '@testing-library/jest-dom';
+import { DEFAULT_CATEGORY } from 'shared/consts';
 import { vi } from 'vitest';
 
 beforeAll(() => {
-  vi.mock('next/router', () => require('next-router-mock'));
+  vi.mock('next/navigation', async (importOriginal) => {
+    const actual = await importOriginal<typeof import('next/navigation')>();
+    const { useRouter } =
+      await vi.importActual<typeof import('next-router-mock')>(
+        'next-router-mock'
+      );
+    const usePathname = vi.fn().mockImplementation(() => {
+      return `/${DEFAULT_CATEGORY}`;
+    });
+    return {
+      ...actual,
+      useRouter: vi.fn().mockImplementation(useRouter),
+      usePathname,
+    };
+  });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         '**/src/entities/Items/model/types/**',
         '**/src/vite-env.d.ts',
         '**/src/shared/types/**',
+        '**/index.ts',
       ],
     },
     globals: true,
