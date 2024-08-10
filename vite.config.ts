44ec734645dc2291daea7addf9b071c072e3293a
rRedq <redq_000@mail.ru>
2024-08-10 17:38:27 +0500

    chore: set up remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 /// <reference types="vitest" />
+import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+// export default defineConfig({
+//   plugins: [react(), tsconfigPaths()],
+//   test: {
+//     coverage: {
+//       provider: 'v8',
+//       reporter: ['text'],
+//     },
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: ['./setupTests.ts'],
+//   },
+// });
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    coverage: {
-      provider: 'v8',
-      reporter: ['text'],
-    },
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./setupTests.ts'],
-  },
+  plugins: [
+    remix({
+      appDirectory: 'src/app',
+    }),
+    tsconfigPaths(),
+  ],
 });
