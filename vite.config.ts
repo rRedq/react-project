3402773c1095e37443ba7006b3ab7562f43f65d3
rRedq <redq_000@mail.ru>
2024-08-10 17:42:45 +0500

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,6 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// export default defineConfig({
-//   plugins: [react(), tsconfigPaths()],
-//   test: {
-//     coverage: {
-//       provider: 'v8',
-//       reporter: ['text'],
-//     },
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: ['./setupTests.ts'],
-//   },
-// });
 export default defineConfig({
   plugins: [
     remix({
