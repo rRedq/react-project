ec73dd1e8f8d6b3347072685baa5948790762bbc
rRedq <redq_000@mail.ru>
2024-08-11 19:46:15 +0500

    refactor: update root test

--- src/__test-server__/root.test.tsx
+++ src/__test-server__/root.test.tsx
@@ -1,20 +1,15 @@
 import { createRemixStub } from '@remix-run/testing';
 import { render } from '@testing-library/react';
-import App, { ErrorBoundary } from 'app/root';
+import App from 'app/root';
 import MainPage from 'app/routes/home.$category';
 import { CoreProvider } from 'core';
 import { path } from 'shared/lib/__mock__';
-import { Paths } from 'shared/types';
 
 test('a', async () => {
   const MockApp = createRemixStub([
     {
       path: '/',
       Component: App,
-    },
-    {
-      path: Paths.NOT_FOUND,
-      Component: ErrorBoundary,
     },
     {
       path,
@@ -24,7 +19,7 @@ test('a', async () => {
 
   render(
     <CoreProvider>
-      <MockApp initialEntries={['/', Paths.NOT_FOUND, path]} />
+      <MockApp initialEntries={['/', path]} />
     </CoreProvider>
   );
 });
